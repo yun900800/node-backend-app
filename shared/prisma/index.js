2b28bd6c89fa22062ca97952cba272d97a1bf5a5
yun900800@126.com <yun900800@126.com>
2025-10-07 17:45:58 +0800

    [bug] 强制使用wasm引擎

--- shared/prisma/index.js
+++ shared/prisma/index.js
@@ -1,4 +1,6 @@
 import { PrismaClient } from '../generated/prisma-client/index.js';
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  engine: 'wasm', // 强制使用 WASM 引擎
+});
 
 export { prisma };
